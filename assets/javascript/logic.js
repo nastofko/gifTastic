3f4743b803eb38648fe6ce79911bf64b145cf1e0
unknown <nastofko@gmail.com>
2018-11-27 00:07:52 -0700

    with background

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,7 +7,7 @@ function renderButtons() {
     $("#btn-container").empty();
 
     for (var i = 0; i < topics.length; i++) {
-        var btn = $('<button>');
+        var btn = $('<button>').css('background-color', 'white','color', 'red' );
         btn.addClass("btn-topic");
         btn.text(topics[i])
         btn.attr("data-topic", topics[i])
@@ -42,32 +42,43 @@ function topicAPI(event) {
         console.log(results)
 
         //for loop to create div
-        // create <p> for rating
+
         for (let i = 0; i < results.length; i++) {
             var imageDiv = $("<div>");
 
+            
+            //NEEDS WORK
+            var rating = results.Rated;
+            var pOne = $("<p>").text("Rating: " + rating).css('color', 'red');
             var image = $("<img>");
+            imageDiv.append(pOne);
 
             image.attr("src", results[i].images.original.url);
             console.log(results[i].images.original.url)
 
-            // still and animate gif
-
-
+            
+            
             // imageDiv.append(p);
             imageDiv.append(image);
             $("#results").append(imageDiv);
         }
+        // still and animate gif
+        
+        $(".gifs").on("click", function () {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
 
-
-        // for (var i = 0; i < results.length; i++) {
-
-
-
-
-
-
-        // }
 
 
     })
@@ -92,3 +103,8 @@ renderButtons();
 
 
 
+// need to fix ratings
+// fix still gifs
+//fix all button colors
+// no empty boxes
+//different random gif everytime
